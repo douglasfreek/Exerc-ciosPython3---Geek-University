c21e986effe06b5ec74c4da2c7301ee6672e7eba
Douglas dos Santos Batista <douglassantosbt1@gmail.com>
2023-04-03 22:11:45 -0300

    resolução dos exercícios da seção 7, parte 2

---
+++ seção 7/exercícios/parte 2/5.py
@@ -0,0 +1,42 @@
+"""
+Leia uma matriz 5 x 5. Leia também um valor X. O programa deverá fazer uma busca do valor X na matriz.
+Ao final deverá escrever a localização (linha e coluna) do valor, ou uma mensagem de não encontrado.
+"""
+from random import randint
+
+matriz = []
+find = False
+coord = ()
+
+for x in range(5):
+    linha = []
+    while len(linha) < 5:
+        for y in range(5):
+            linha.append(randint(0, 99))
+    matriz.append(linha)
+print(f'\n{"matriz 5 x 5":^21}\n')
+
+for i, x in enumerate(matriz):
+    for c, y in enumerate(x):
+        print(f'{y:>3}', end=' ')
+    print(end='\n')
+
+while True:
+    try:
+        valor_x = int(input('\nQual número inteiro deseja procurar na matriz?\n\033[32m>\033[m '))
+    except:
+        print('\033[31m> erro :\033[37m valor inválido\033[m')
+    else:
+        break
+
+for c, x in enumerate(matriz):
+    for i, y in enumerate(x):
+        if y == valor_x and len(coord) == 0:
+            find = True
+            coord = c + 1, i + 1
+
+if find:
+    print(f'\033[32m>\033[m A primeira aparição do \033[32m{valor_x}\033[m foi na linha \033[32m{coord[0]}'
+          f'\033[m coluna \033[32m{coord[1]}\033[m')
+else:
+    print(f'\033[32m>\033[m O número \033[32m{valor_x}\033[m não está na matriz.')
