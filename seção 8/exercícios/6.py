0cc806622dd7a62d1cc19d8e9c2dc5038b98344c
douglas <douglassantosbt1@gmail.com>
2023-04-08 01:20:41 -0300

    resolucao exercicios secao 8 exercício 6.

---
+++ seção 8/exercícios/6.py
@@ -0,0 +1,46 @@
+"""
+Faça uma função que receba 3 números inteiros como parâmetros, representando horas, minutos e segundos.
+Converta-os em segundos.
+"""
+
+
+def convert_segundos(horas=0, minutos=0, segundos=0):
+    """
+    -> Converte as horas, minutos e segundos, em um valor total convertido em segundos.
+    :param horas: valor inteiro para horas
+    :param minutos: valor inteiro para minutos
+    :param segundos: valor inteiro para segundos
+    :return: soma de todos os valores convertidos em segundos.
+    """
+    segundos += minutos * 60
+    segundos += horas * 3600
+    return segundos
+
+
+print('-=-' * 14)
+print(f'{"hours and minutes converter tabajara 1.0":^43}')
+print('-=-' * 14)
+while True:
+    try:
+        horas = int(input('\033[32m>\033[m Valor inteiro de horas: '))
+    except ValueError:
+        print('\033[31merro : valor inválido\033[m')
+    else:
+        break
+while True:
+    try:
+        minutos = int(input('\033[32m>\033[m Valor inteiro de minutos: '))
+    except ValueError:
+        print('\033[31merro : valor inválido\033[m')
+    else:
+        break
+while True:
+    try:
+        segundos = int(input('\033[32m>\033[m Valor inteiro de segundos: '))
+    except ValueError:
+        print('\033[31merro : valor inválido\033[m')
+    else:
+        break
+print(f'\033[32m>\033[m \033[33m{horas}\033[m horas : \033[33m{minutos}\033[m minutos : '
+      f'\033[33m{segundos}\033[m segundos\n'
+      f'\033[32m>\033[m é igual = \033[32m{convert_segundos(horas, minutos, segundos)}\033[m segundos.')
