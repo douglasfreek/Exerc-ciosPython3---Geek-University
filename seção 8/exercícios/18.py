7aefc4f3590c27ae900d9f86f5f14821797cb02f
douglas <douglassantosbt1@gmail.com>
2023-04-19 00:48:25 -0300

    resolução exercícios seção 8 - exercício 18

---
+++ seção 8/exercícios/18.py
@@ -0,0 +1,12 @@
+"""
+Faça uma função que receba por parâmetro dois valores X e Z.
+Calcule e retorne o resultado de X**Z para o programa principal.
+Não utilize nenhuma função pronta de exponenciação.
+"""
+
+
+def expo(x, z):
+    return x ** z
+
+
+print(expo(5, 2))
